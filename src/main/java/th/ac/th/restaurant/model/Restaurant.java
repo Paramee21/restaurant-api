9803664b276ac8369ed00bf8d1e4bc6a0d92c302
Paramee21 <61011359+Paramee21@users.noreply.github.com>
2021-09-30 23:39:13 +0700

    first commit

---
+++ src/main/java/th/ac/th/restaurant/model/Restaurant.java
@@ -0,0 +1,67 @@
+package th.ac.th.restaurant.model;
+
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class Restaurant {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @Type(type = "org.hibernate.type.UUIDCharType")
+    @Column(columnDefinition = "CHAR(36)")
+
+    private UUID id;
+
+    private String name;
+    private String address;
+    private String phone;
+    private int numSeats;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name != null) //check here best
+            this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getNumSeats() {
+        return numSeats;
+    }
+
+    public void setNumSeats(int numSeats) {
+        this.numSeats = numSeats;
+    }
+}
